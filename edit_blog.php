3180af44a8745a396bd3c069c01365ec9680e958
Naimur Rahman Emon <125083056+developer-naimur-rahman@users.noreply.github.com>
2025-11-23 07:22:22 +0600

    new

--- edit_blog.php
+++ edit_blog.php
@@ -82,7 +82,7 @@ if ($logged_in_user && ($logged_in_user === $post['writer'] || $logged_in_user =
             <ul>
                 <li><a href="./index.html">Home</a></li>
                 <li><a href="./about.html">About</a></li>
-                <li><a href="./portfolio.html">Portfolio</a></li>
+                <li><a href="./portfolio.html">Work</a></li>
                 <li><a href="./services.html">Services</a></li>
                 <li><a href="./blog.php" class="active">Blog</a></li>
                 <li><a href="./contact.html">Contact</a></li>
